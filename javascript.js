5d7c4543c2af8d181f172cf2ecd624b231bc29bf
Muthu Chidambaram <Muthu@Muthus-MacBook-Pro.local>
2017-09-22 23:18:50 -0500

    Adding in initialization for firebase

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.4.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyChk2P0D-9bcUXZZQAurXfwnMCXnmvAF60",
+    authDomain: "hackal-608cf.firebaseapp.com",
+    databaseURL: "https://hackal-608cf.firebaseio.com",
+    projectId: "hackal-608cf",
+    storageBucket: "",
+    messagingSenderId: "473729330827"
+  };
+  firebase.initializeApp(config);
+</script>
